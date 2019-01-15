87937a10576cb93dad102c06890ded59cf2733ce
Matthew <mdenslinger@gmail.com>
2019-01-14 21:35:04 -0500

    Create convert to base 10 function

--- script.js
+++ script.js
@@ -16,3 +16,36 @@ const get_values = () => {
 
 	return [num, orig_base, new_base];
 }
+
+const number_base_10 = (number, base) => {
+	/*
+	* Takes user inputted number and the base and converts it to base 10
+	* Returns: number in base 10
+	*/
+
+	let bases_higher_than_10 = {
+		A: 10,
+		B: 11,
+		C: 12,
+		D: 13,
+		E: 14,
+		F: 15,
+		G: 16,
+		H: 17,
+		J: 18,
+		K: 19,
+	};
+
+	let base_10 = 0;
+	number = String(number).toUpperCase();
+
+	for(let i = 0; i < number.length ; i++){
+		if(number[i] in bases_higher_than_10){
+			base_10 += bases_higher_than_10[number[i]]*base**(number.length-(i+1));
+		}
+		else{
+			base_10 += parseInt(number[i])*base**(number.length-(i+1));
+		}
+	}
+	return base_10; 
+}
